2b8d4b732783de9368aa28abd439892aea7f0ad3
Robson Medeiros <robsonmedeiros125@gmail.com>
2026-07-29 12:53:25 -0300

    fix: fixing migrations

--- src/main/resources/db/migration/V1__create_schema.sql
+++ src/main/resources/db/migration/V1__create_schema.sql
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS products(
   price NUMERIC(7, 2) NOT NULL CHECK(price >= 0),
   available BOOLEAN DEFAULT true,
   preparation_minutes INTEGER,
-  stock INTEGER,
+  stock INTEGER DEFAULT 0,
   created_at TIMESTAMP DEFAULT NOW(),
   updated_at TIMESTAMP,
 
--- src/main/resources/db/migration/V2__insert_data_seed.sql
+++ src/main/resources/db/migration/V2__insert_data_seed.sql
@@ -5,18 +5,18 @@ INSERT INTO categories_products(name)
 VALUES ('Bebidas'), ('Sobremesas'), ('Salgados'), ('Hamburguers'),
 ('Pizzas');
 
-INSERT INTO products(category_id, name, description, price, preparation_minutes)
-SELECT category.id, 'X-Bacon', 'Hamburguer com bacon e queijo cheddar', 26.99, 25
+INSERT INTO products(category_id, name, description, price, preparation_minutes, created_at)
+SELECT category.id, 'X-Bacon', 'Hamburguer com bacon e queijo cheddar', 26.99, 25, now()
 FROM categories_products category WHERE category.name = 'Hamburguers';
 
-INSERT INTO products(category_id, name, description, price, preparation_minutes)
-SELECT category.id, 'Pizza Calabresa', 'Pizza com calabresa e cebola', 40.50, 35
+INSERT INTO products(category_id, name, description, price, preparation_minutes, created_at)
+SELECT category.id, 'Pizza Calabresa', 'Pizza com calabresa e cebola', 40.50, 35, now()
 FROM categories_products category WHERE category.name = 'Pizzas';
 
-INSERT INTO products(category_id, name, description, price, preparation_minutes)
-SELECT category.id, 'Suco de Laranja', 'Suco de laranja 250ml', 10.00, 10
+INSERT INTO products(category_id, name, description, price, preparation_minutes, created_at)
+SELECT category.id, 'Suco de Laranja', 'Suco de laranja 250ml', 10.00, 10, now()
 FROM categories_products category WHERE category.name = 'Bebidas';
 
-INSERT INTO products(category_id, name, description, price)
-SELECT category.id, 'Coxinha', 'Coxinha de Frango', 8.00
+INSERT INTO products(category_id, name, description, price, created_at)
+SELECT category.id, 'Coxinha', 'Coxinha de Frango', 8.00, now()
 FROM categories_products category WHERE category.name = 'Salgados';
